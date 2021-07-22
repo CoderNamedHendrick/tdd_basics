63332ac414abcbb1b145fe45572c4e7382fd6e2b
Hendrick <sebastinesoacatp@gmail.com>
2021-07-23 00:11:01 +0100

    pass the test

---
+++ lib/user_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+class UserModel {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  UserModel({
+    @required this.userId,
+    @required this.id,
+    @required this.title,
+    @required this.body,
+  });
+}
--- test/user_test.dart
+++ test/user_test.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd_tut/user_model.dart';
 
 void main() {
   test('test 1', () {
